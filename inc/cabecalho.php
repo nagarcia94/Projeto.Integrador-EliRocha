96e1e516f445bec19929f480941e3bd781f6d645
nagarcia94 <000nagarcia@gmail.com>
2023-11-18 20:01:43 -0300

    cabeçalho

--- inc/cabecalho.php
+++ inc/cabecalho.php
@@ -4,6 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eli Rocha - Nutrição</title>
+   
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+<link href="css/style.css">
+
+
+
 </head>
 <body class="d-flex flex-column h-100">
 
@@ -36,6 +46,10 @@
 </nav>
 
 </header>
+
+<main class="flex-shrink-0">
+    <div class="container">
+
     
 
 
